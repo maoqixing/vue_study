00bbbbb1597ef2f6d58e5e930d60c179b062e790
毛其兴 <879652938@qq.com>
2020-06-20 11:53:04 +0800

    mqx

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,6 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Mine from '../views/Mine.vue'
 Vue.use(VueRouter)
-
-let func = ({params,query})=>{
-	return {
-		name:params.name,
-		sex:params.sex,
-		height:query.height,
-		dog:query.dog,
-	}
-};
-
   const routes = [
 //	{ path: '/', redirect: '/home' },//重定向1
 // { path: '/', redirect: {name:'About'} },//重定向2
@@ -34,32 +24,13 @@ let func = ({params,query})=>{
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
-	//   { 通过路由传参
-	//   path: '/Mine/:name/:sex',   //http://localhost:8080/mine/%E5%BC%A0%E4%B8%89/%E7%94%B7?height=110&dog=%E5%9C%9F%E8%B1%86
-	//   name: 'Mine',
-	//   component: Mine
-	// },
-	
-	//   { 	// 通过属性传参
-	//   path: '/Mine',   //http://localhost:8080/mine/%E5%BC%A0%E4%B8%89/%E7%94%B7?height=110&dog=%E5%9C%9F%E8%B1%86
-	//   name: 'Mine',
-	//   component: Mine,
-	// 	props:{name:'小廖'}
-	// },
-	
-	// 	  { 	// 通过属性传参
-	//   path: '/Mine/:name/:sex',   //http://localhost:8080/mine/%E5%BC%A0%E4%B8%89/%E7%94%B7?height=110&dog=%E5%9C%9F%E8%B1%86
-	//   name: 'Mine',
-	//   component: Mine,
-	// 	props:true
-	// },
-	
-			  { 	// 通过属性传参
-	  path: '/Mine/:name/:sex',   //http://localhost:8080/mine/%E5%BC%A0%E4%B8%89/%E7%94%B7?height=110&dog=%E5%9C%9F%E8%B1%86
+	{ 
+	  path: '/Mine', 
 	  name: 'Mine',
-	  component: Mine,
-		props:func
+	  component: Mine
 	},
+
+
 ]
 
 const router = new VueRouter({
